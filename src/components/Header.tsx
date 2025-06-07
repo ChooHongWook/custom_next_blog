8f18d80ef18dfea6d94c9ca3da1211ee8c3a9915
Hong Wook Choo <hw.choo@amicuslex.net>
2025-06-08 08:25:12 +0900

    feat(header): uncomment blog navigation item in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
         <h1 className="text-xl font-bold text-gray-900">개발 블로그</h1>
         <nav className="flex gap-6">
           <HeaderNavItem href="/">홈</HeaderNavItem>
-          {/* <HeaderNavItem href="/blog">블로그</HeaderNavItem> */}
+          <HeaderNavItem href="/blog">블로그</HeaderNavItem>
           {/* <HeaderNavItem href="#">카테고리</HeaderNavItem> */}
           {/* <HeaderNavItem href="#">소개</HeaderNavItem> */}
           {/* <HeaderNavItem href="#">연락처</HeaderNavItem> */}
